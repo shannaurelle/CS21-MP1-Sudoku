dd5cc8aa84ad36ad60c9265eb2773c4a5aa660a9
shannaurelle <shannaurelleg@gmail.com>
2022-04-16 15:14:29 +0800

    updated changes

--- sudoku4x4.asm
+++ sudoku4x4.asm
@@ -3,52 +3,116 @@
 # sudoku4x4.asm -- program to solve a 4x4 sudoku board
 .text
 main:
-    # get input four times
-    li $t0, 0
-    li $t1, 20
-input_loop:
+    # store the row address to global ptr gp
+    la $t0, row_1
+    sw $t0, 0($gp)
+    la $t0, row_2
+    sw $t0, 4($gp)
+    la $t0, row_3
+    sw $t0, 8($gp)
+    la $t0, row_4
+    sw $t0, 12($gp)
+
+    # row sets for algorithm
+    la $t0, row_set_1
+    sw $t0, 16($gp)
+    la $t0, row_set_2
+    sw $t0, 20($gp)
+    la $t0, row_set_3
+    sw $t0, 24($gp)
+    la $t0, row_set_4
+    sw $t0, 28($gp)
+
+    # column sets for algorithm
+    la $t0, col_set_1
+    sw $t0, 32($gp)
+    la $t0, col_set_2
+    sw $t0, 36($gp)
+    la $t0, col_set_3
+    sw $t0, 40($gp)
+    la $t0, col_set_4
+    sw $t0, 44($gp)
+
+    # region sets for algorithm
+    la $t0, region_set_1
+    sw $t0, 48($gp)
+    la $t0, region_set_2
+    sw $t0, 52($gp)
+    la $t0, region_set_3
+    sw $t0, 56($gp)
+    la $t0, region_set_4
+    sw $t0, 60($gp)
+
     # get input string
-    la $a0, string
-    li $a1, 4
+    move $t0, $zero
+    li $s0, 16
+input_loop:
+    addu $t1, $t0, $gp 
+    lw $t1, 0($t1)
+    la $a0, 0($t1)
+    li $a1, 5 
     li $v0, 8
     syscall
-
-    # copy the string character by character, then store to global gp pointer
-    move $t3, $a0
-    move $t4, $gp
-    add  $t4, $t4, $t0 
-copy_loop:
-    lbu  $t5, 0($t3)
-    sb   $t5, 0($t4)
-    addi $t3, $t3, 1
-    addi $t4, $t4, 1
-    bnez $t5, copy_loop # stop copying when null character reached
-    addi $t4, $t4, 1
-    sb $zero, 0($t4)       # add the null terminator
-
-    addi $t0, $t0, 5
-    blt $t0,$t1,input_loop
-
+    addiu $t0, $t0, 4 
+    blt $t0,$s0,input_loop
+    
     li $a0, 10
     li $v0, 11
-    syscall             # print newline 
+    syscall             # print newline
 
+    # encode the input to the sets before running the algorithm
     move $t0, $zero
-    move $t3, $gp
+    li $s0, 16
+encode_loop:
+    addu $t1, $t0, $gp 
+    lw $t1, 0($t1)
+    la $a0, 0($t1)
+    move $t1, $zero
+char_encode_loop:
+    lw $t2, 0($a0)
+    addu $t2, $t2, $t1
+    lbu $t3, 0($t2)
+    
+    addiu $t1, $t1, 1
+    blt $t1,4,char_encode_loop
+    addiu $t0, $t0, 4
+    blt $t0,$s0,encode_loop
+
+    # print stored string
+    move $t0, $zero
+    li $s0, 16
 print_loop:
-    move $a0, $t3
-    li $v0, 4           # for print string syscall
-    syscall             # print string
+    addu $t1, $t0, $gp 
+    lw $t1, 0($t1)
+    la $a0, 0($t1)
+    li $v0, 4
+    syscall
 
     li $a0, 10
     li $v0, 11
-    syscall             # print newline 
+    syscall             # print newline
 
-    add $t3, $t3, 5
-    add $t0, $t0, 5
-    blt $t0, $t1, print_loop 
+    addiu $t0, $t0, 4 
+    blt $t0,$s0,print_loop
+    
 exit:
     li $v0, 10
     syscall
 .data
-    string: .space 8    # reserve 8 bytes into memory 
+    string: .space 8    # reserve 8 bytes into memory
+    row_1:  .space 8
+    row_2:  .space 8
+    row_3:  .space 8 
+    row_4:  .space 8
+    row_set_1: .space 4
+    row_set_2: .space 4
+    row_set_3: .space 4
+    row_set_4: .space 4
+    col_set_1: .space 4
+    col_set_2: .space 4
+    col_set_3: .space 4
+    col_set_4: .space 4
+    region_set_1: .space 4
+    region_set_2: .space 4
+    region_set_3: .space 4
+    region_set_4: .space 4
