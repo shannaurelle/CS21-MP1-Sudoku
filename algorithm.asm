b9e7eecf009643820d5d171126a323cbaa77fdc5
shannaurelle <shannaurelleg@gmail.com>
2022-04-23 19:13:56 +0800

    partial progress v2

--- algorithm.asm
+++ algorithm.asm
@@ -1,8 +1,11 @@
 # a0, a1, a2, (row, column, value)
 sudoku:
     ### PREAMBLE ###
-    subu $sp, $sp, 4
+    subu $sp, $sp, 16
     sw $ra, 0($sp)  # 
+    sw $a0, 4($sp)
+    sw $a1, 8($sp)
+    sw $a2, 12($sp)
     ### PREAMBLE ###
     move $t0, $zero
     move $t1, $zero
@@ -22,9 +25,6 @@ accumulate:
     jal print_board
     j exit
 sudoku_recurse:
-    move $s0, $a0
-    move $s1, $a1
-    move $s2, $a2
     move $s3, $zero
 
     #jal check_set
@@ -32,12 +32,12 @@ sudoku_recurse:
     jal get_cell
     move $a2, $v0
     jal check_cell
-    move $a2, $s2
+    lw $a2, 12($sp)
 
     bnez $v0,skip_cell
     addiu $a2, $a2, 1   # value = value + 1
     jal sudoku
-    move $a2, $s2
+    lw $a2, 12($sp)
 skip_cell:
     jal set_cell
     move $s3, $v0       # s3 = former cell
@@ -45,18 +45,18 @@ skip_cell:
 
     addiu $a0, $a0, 1   # row = row + 1
     jal sudoku
-    move $a0, $s0
+    lw $a0, 4($sp)
     addiu $a1, $a1, 1   # col = col + 1
     jal sudoku
-    move $a1, $s1
+    lw $a1, 8($sp)
     
+    jal delete_to_set
     move $a2, $s3
     jal insert_to_set      # backtrack
-    move $a2, $s2
-    jal delete_to_set
+    
 sudoku_end:
     ### END ###
     lw $ra, 0($sp)
-    addu $sp, $sp, 4
+    addu $sp, $sp, 16
     ### END ###
     jr $ra
